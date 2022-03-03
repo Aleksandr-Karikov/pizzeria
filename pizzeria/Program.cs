12c5e6813fa8dc76e6bd3f2abd03774d47a4a649
Aleksandr <a.karikov@powwwer.io>
2022-03-03 22:18:15 +0700

    first commit

---
+++ pizzeria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pizzeria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
